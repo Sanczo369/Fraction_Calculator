2e1a51e6dc78bd53b8723481579da86d0256e53c
Arek Sanecki <areksanecki@gmail.com>
2023-03-17 00:25:26 +0100

    add __add__ methods + Remove redundant parentheses

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class Fraction:
     # display Fraction
     def __str__(self):
         self.simplyfy()
-        return ("{}/{}".format(self.numerator,self.denominator))
+        return "{}/{}".format(self.numerator, self.denominator)
 
     # simplyfy Fraction
     def simplyfy(self):
@@ -16,4 +16,15 @@ class Fraction:
                 self.numerator = int(self.numerator / divisor)
                 self.denominator = int(self.denominator / divisor)
                 return self.simplyfy()
-        return (self.numerator, self.denominator)
+        return self.numerator, self.denominator
+
+    # Addition + (with magic methods __add__())
+    def __add__(self, number2):
+        if self.denominator != number2.denominator:
+            result_n = self.numerator * number2.denominator + number2.numerator * self.denominator
+            result_d = self.denominator * number2.denominator
+            return Fraction(result_n, result_d)
+        else:
+            result_n = self.numerator + number2.numerator
+            result_d = self.denominator
+            return Fraction(int(result_n), int(result_d))

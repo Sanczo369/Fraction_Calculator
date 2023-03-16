346803cc0d95b35fea4a2b182e53bc5af75cacdf
Arek Sanecki <areksanecki@gmail.com>
2023-03-16 18:27:22 +0100

    add Simplyfy Function to main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ class Fraction:
 
     # display Fraction
     def __str__(self):
+        self.simplyfy()
         return ("{}/{}".format(self.numerator,self.denominator))
 
-
+    # simplyfy Fraction
+    def simplyfy(self):
+        min_val = min(self.numerator, self.denominator)
+        for divisor in range(2, min_val + 1):
+            if (self.numerator % divisor == 0) and (self.denominator % divisor == 0):
+                self.numerator = int(self.numerator / divisor)
+                self.denominator = int(self.denominator / divisor)
+                return self.simplyfy()
+        return (self.numerator, self.denominator)
